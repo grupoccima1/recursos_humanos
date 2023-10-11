748ccbe6ae1f1aa0bd1a46eb1fc5bebcdbaca6da
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-10-11 17:08:48 -0600

    Add files via upload

--- validacion.php
+++ validacion.php
@@ -4,12 +4,11 @@ include './class/conexion.php';
 $correo = $_POST['exampleInputEmail1'];
 $pass = $_POST['exampleInputPassword1'];
 
-// echo $correo;
-// echo $pass;
+
 
 $les="SELECT count(*) FROM usuarios WHERE correo = '$correo' AND password = '$pass'";
 $ser=mysqli_query($conexion,$les);
-$som = mysqli_fetch_row($ser);
+$som = mysqli_fetch_row($ser); 
 
 $les1="SELECT * FROM usuarios WHERE correo = '$correo' AND password = '$pass'";
 $ser1=mysqli_query($conexion,$les1);
@@ -21,7 +20,7 @@ $som1 = mysqli_fetch_row($ser1);
         switch ($som1[4]) {
             case '1':
                 session_start();
-                $_SESSION['usuario'] = $som1[1];
+                $_SESSION['usuario'] = $som1[1]; 
                 header('Location: ./master/dashboard_master.php');
             break;
             case '2':

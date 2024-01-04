89f9e1a07027b8b33ace11ad6d8ccd500a0c54ca
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2024-01-04 11:32:37 -0600

    Add files via upload

--- class/conexion.php
+++ class/conexion.php
@@ -1,5 +1,8 @@
 <?php
-    $conexion=mysqli_connect('localhost','root','','recursoshumanos');
+    //$conexion=mysqli_connect('localhost','root','','recursoshumanos');
+    //  conexion en linea 
+      $conexion=mysqli_connect('localhost','bbbme11_sistemas','Gccima23.','bbbme11_recursosh');
+
 ?>
 
 
